fd247419b44fd9361ada2828aefad30def76486a
Cabbage <88053948+cabbageofficial198@users.noreply.github.com>
2023-02-22 19:48:08 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  await app.listen(3001);
+  await app.listen(5000);
 }
 bootstrap();
